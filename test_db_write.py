5dea09eef39c026133890d06d325cc857f28593c
patrickellis <patrick.ellis.w1@gmail.com>
2026-06-06 19:31:05 +0100

    chore: remove ad-hoc testing scripts

--- test_db_write.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.abspath("src"))
-from core.db import mark_evaluated
-
-# Try writing to a dummy property "TEST1234"
-print("Calling mark_evaluated...")
-mark_evaluated("TEST1234", ignored=True, score=0.0, breakdown={"pros": []})
-print("Done!")
